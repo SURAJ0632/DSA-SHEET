da1f4adf958c143c19173effebb34c7536862114
suraj0632 <thakursuraj7009@gmail.com>
2025-09-05 21:22:17 +0530

    zigzag done

---
+++ whileLoop.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int i=1;
+    int n=10;
+
+    int sum=0;
+
+    while(i<=n){
+        sum=sum+i;
+        i=i+1;
+    }
+
+    cout<<sum<<endl;
+}
