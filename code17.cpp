fe1c64793a5aa3a8e835c53d1a09ca1311fa8d05
suraj0632 <thakursuraj7009@gmail.com>
2025-09-11 22:19:28 +0530

    new unit

--- code17.cpp
+++ code17.cpp
@@ -1,35 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
+    vector<string> letterCombinations(string digits)
+    {
+        if (digits.empty())
+            return {};
 
         vector<string> mapping = {
-            "",    // 0
-            "",    // 1
-            "abc", // 2
-            "def", // 3
-            "ghi", // 4
-            "jkl", // 5
-            "mno", // 6
-            "pqrs",// 7
-            "tuv", // 8
-            "wxyz" // 9
-        };
+            "",
+            "",
+            "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
         vector<string> result;
         string current;
 
-        function<void(int)> backtrack = [&](int index) {
-            if (index == digits.size()) {
+        function<void(int)> backtrack = [&](int index)
+        {
+            if (index == digits.size())
+            {
                 result.push_back(current);
                 return;
             }
 
             string letters = mapping[digits[index] - '0'];
-            for (char c : letters) {
+            for (char c : letters)
+            {
                 current.push_back(c);
                 backtrack(index + 1);
                 current.pop_back();
@@ -41,7 +39,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
     string digits;
     cout << "Enter digits (2-9): ";
@@ -50,7 +49,8 @@ int main() {
     vector<string> ans = sol.letterCombinations(digits);
 
     cout << "Letter combinations are:\n";
-    for (auto &s : ans) {
+    for (auto &s : ans)
+    {
         cout << s << " ";
     }
     cout << endl;
