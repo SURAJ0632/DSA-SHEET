ff9ff63835c7a8fbbe1e9b68c00036ebe53c4f8d
suraj0632 <thakursuraj7009@gmail.com>
2025-09-17 21:35:30 +0530

    new commit

---
+++ 367.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 2) return true;
+
+        long long left = 2, right = num / 2;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long guessSquared = mid * mid;
+
+            if (guessSquared == num) {
+                return true;
+            } else if (guessSquared > num) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
+
+// Example usage
+int main() {
+    Solution sol;
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (sol.isPerfectSquare(num)) {
+        cout << num << " is a perfect square." << endl;
+    } else {
+        cout << num << " is not a perfect square." << endl;
+    }
+
+    return 0;
+}
